b4fce6a96cc8ab603b026559f4d87694919fa4ed
Aditya-KNV <knvaditya06@gmail.com>
2025-09-09 21:05:03 +0530

    Update HospitalDemo.java

--- Assignment_9Sep/HospitalDemo.java
+++ Assignment_9Sep/HospitalDemo.java
@@ -1,3 +1,11 @@
+// Assignment_9Sep:
+// Download the HospitalDemo.java
+// javac HospitalDemo.java
+// java HospitalDemo
+
+// This will run the code and give the desired output.
+
+
 import java.util.ArrayList;
 import java.util.List;
 
